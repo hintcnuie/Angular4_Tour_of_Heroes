d5e443a552e78aae65c17bdd765ae464dc07f900
Charley Chen <zwchenbj@cn.ibm.com>
2017-04-04 14:20:00 +0800

    init commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector:'my-app',
+    templateUrl:'./app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    title = '中国古代十大名将';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule }        from '@angular/platform-browser';
+import { NgModule }             from '@angular/core';
+import { FormsModule }          from '@angular/forms';
+import { HttpModule }           from '@angular/http';
+import { RouterModule }         from '@angular/router';
+
+import { AppComponent }         from './app.component';
+import { HeroesComponent }      from './heroes/heroes.component';
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+
+import { HeroService }          from './hero.service';
+
+import { AppRoutingModule }     from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent
+    
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
+  ],
+  providers: [HeroService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
